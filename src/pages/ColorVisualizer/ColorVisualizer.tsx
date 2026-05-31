d7134452f2054889fb87495878a24ee012b8ab80
Stefano Amenta <git.stefano@outlook.it>
2026-05-31 19:16:18 +0200

    feat: agguinto sound visualizer per poter studiare useContext

--- src/pages/ColorVisualizer/ColorVisualizer.tsx
+++ src/pages/ColorVisualizer/ColorVisualizer.tsx
@@ -1,11 +1,17 @@
 import { ColorInput } from "./ColorInput";
 import { ColorShower } from "./ColorShower";
+import { SoundVisualilzer } from "./SoundVisualizer";
 
 export function ColorVisualizer() {
     return (
-        <div className="color-container">
-            <ColorInput />
-            <ColorShower />
-        </div>
+        <>
+            <div className="color-container">
+                <ColorInput />
+                <ColorShower />
+            </div>
+            <div className="sound-container">
+                <SoundVisualilzer />
+            </div>
+        </>
     );
 }
